5e0d151baeee049a2627c87b7b6286ed5f1d8379
JoseIgnacio2020 <jose.ignacio.silva2015@gmail.com>
2024-06-15 19:31:05 -0400

    Se agregan scripts SQL para la creacion de las tablas.

---
+++ SQL/scripts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE periodistas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE noticias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    categoria VARCHAR(50) NOT NULL,
+    titulo VARCHAR (250) NOT NULL,
+    descripcion VARCHAR (500) NOT NULL,
+     FOREIGN KEY (id_periodista) REFERENCES periodistas(id)
+);
